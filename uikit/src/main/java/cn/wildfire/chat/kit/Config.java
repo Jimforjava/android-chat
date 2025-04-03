be89a16aacac4dfac364f5e8a9b4408c0b4aba7c
Jimforjava <gdesiretom@gmail.com>
2025-04-04 02:20:07 +0800

    Update Config.java
    修改了遗漏结束符的问题

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -75,7 +75,7 @@ public class Config {
      * 组织通讯录服务地址，如果没有部署，可以设置为null
      */
     //public static String ORG_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "https://org.wildfirechat.cn";
-    public static String ORG_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = null
+    public static String ORG_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = null;
     /**
      * 发送日志命令，当发送此文本消息时，会把协议栈日志发送到当前会话中，为空时关闭此功能。
      */
